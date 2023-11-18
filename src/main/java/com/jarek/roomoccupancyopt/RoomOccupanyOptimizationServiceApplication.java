14fce4a89fafb0835bffd791b01b00b788b8a5eb
jarek <jarek@sureclinical.org>
2023-11-18 21:42:34 +0100

    Initial commit - project setup

---
+++ src/main/java/com/jarek/roomoccupancyopt/RoomOccupanyOptimizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jarek.roomoccupancyopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupanyOptimizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupanyOptimizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarek/roomoccupancyopt/RoomOccupanyOptimizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarek.roomoccupancyopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupanyOptimizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
